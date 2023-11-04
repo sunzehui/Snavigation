49e6237b0d843a4327755cbd99a3835b34e45889
孙泽辉 <501574022@qq.com>
2023-11-04 12:21:14 +0800

    chore: 优化打包

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { VitePWA } from "vite-plugin-pwa";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import viteCompression from "vite-plugin-compression";
-
+import legacy from '@vitejs/plugin-legacy'
+import browserslistToEsbuild from 'browserslist-to-esbuild'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -49,8 +50,20 @@ export default defineConfig({
         ],
       },
     }),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
     // viteCompression
-    viteCompression(),
+    viteCompression({
+      // 可选配置项
+      ext: '.gz', // 压缩文件的扩展名，默认为 .gz
+      algorithm: 'gzip', // 压缩算法，可以是 'gzip' 或 'brotliCompress'
+      threshold: 10240, // 超过多少字节的文件才会被压缩，默认为 0
+      deleteOriginFile: false, // 是否删除原始文件，默认为 false
+      verbose: true, // 是否在控制台输出压缩结果，默认为 false
+      filter: /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i, // 指定哪些资源不被压缩，默认为 /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
+      disable: false, // 是否禁用插件，默认为 false
+    }),
   ],
   server: {
     port: 5588,
@@ -64,11 +77,31 @@ export default defineConfig({
   },
   build: {
     minify: "terser",
+    target: browserslistToEsbuild(),
     terserOptions: {
       compress: {
         // 生产环境时移除 console
         pure_funcs: ["console.log"],
       },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          try {
+            if (id.includes("node_modules")) {
+              let name = id.split("node_modules/")[1].split("/");
+
+              if (name[0].includes('naive-ui') ){
+                return 'naive-ui'
+              }else{
+                return 'vendor'
+              }
+           }
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      }
     },
   },
 });
